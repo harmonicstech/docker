82b234051762cf02a6019569be18948d4e15c432
harmonicstech <88635438+harmonicstech@users.noreply.github.com>
2023-01-29 19:02:26 +0100

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -6,7 +6,7 @@ echo "Install needed"
 apt-get install ca-certificates curl gnupg lsb-release -y
 
 echo "Add Docker’s official GPG key"
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
